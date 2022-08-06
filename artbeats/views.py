e69b1cc31261159176999cfd3c3815b5416f4e52
Eyob Woldemichael <eyob@uwindsor.ca>
2022-08-06 16:39:47 -0400

    add new lines

--- artbeats/views.py
+++ artbeats/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
+def index(request):
+    return HttpResponse("Hello world!")
 # Create your views here.
 #what is going on here
 #hello
